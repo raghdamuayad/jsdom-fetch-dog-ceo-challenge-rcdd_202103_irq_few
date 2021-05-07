256463abef1ab1bddd8b2ffbe1f93e25f869e8a0
Raghda  Muayad <therose000777@gmail.com>
2021-05-07 07:22:13 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+  fetch(imgUrl)
+  .then(response => response.json())
+  .then(result => {
+    dogImages(result.message)
+  })
+
+  function dogImages(dogs){
+    console.log(dogs)
+    dogs.forEach(dog => {
+      console.log(dog)
+      const goodBoyContainer = document.querySelector('#dog-image-container')
+      const goodBoy = document.createElement('img')
+      goodBoy.src = dog
+      goodBoyContainer.appendChild(goodBoy)
+    });
+  }
